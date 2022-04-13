72620d32287b46f162a41a0a45bfc633cadc9c39
lillian-bentley <49650492+lillian-bentley@users.noreply.github.com>
2022-04-13 10:04:27 -0600

    print
    worked on getting the print preview going

--- main.java
+++ main.java
@@ -5,6 +5,9 @@
  */
 package groupproject1;
 
+import java.io.File;
+import java.util.Scanner;
+
 /**
  *
  * @author lillianbentley
@@ -14,8 +17,19 @@ public class main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws Exception {
+
+        File file = new File("/Users/lillianbentley/esof322/esof322_groupProject1/notes.txt");
+        Scanner sc = new Scanner(file);
+        StringBuilder str = new StringBuilder();
+
+        while (sc.hasNextLine())
+            str.append(sc.nextLine());
+
+        String send = str.toString();
+        //System.out.println(sc.nextLine());
+
+        print notes = new print(send);
     }
-    
+
 }
